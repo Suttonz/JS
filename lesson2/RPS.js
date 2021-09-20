13f22b47b0665ad57394671a81e17d2ba3ac9f7c
Sutton Zhang <suttonzq@gmail.com>
2021-09-20 18:38:07 -0400

    refactor RPS

--- lesson2/RPS.js
+++ lesson2/RPS.js
@@ -1,33 +1,31 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = {
-  r:'rock',
-  p:'paper',
-  sc:'scissors',
-  l:'lizard',
-  sp:'spock'
-};
-const VALID_CHOICES_KEYS = Object.keys(VALID_CHOICES);
-const WINNING_POSSIBILITIES = {
-  rock : ['lizard','scissors'],
-  paper: ['rock','sopck'],
-  scissors: ['lizard','paper'],
-  spock:['rock','scissors'],
-  lizard :['paper','spock']
-};
-let computerScore = 0;
-let playerScore = 0;
+const CHOICES = {
+  rock: { shorthand: ['r','rock','ROCK'], beats: ['scissors', 'lizard'] },
+  paper: { shorthand: ['p','paper','PAPER'], beats: ['rock', 'spock'] },
+  scissors: { shorthand: ['sc','scissors','SCISSORS'], beats: ['paper', 'lizard'] },
+  lizard: { shorthand: ['l','lizard','LIZARD'], beats: ['spock', 'paper'] , },
+  spock: { shorthand: ['sp','spock','SPOCK'], beats: ['scissors', 'rock'] }
+}
+
+let score = {
+  player: 0,
+  computer: 0
+}
+
+const Max_Score = 3;
 let roundNum = 0;
+
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function decideWinner(choice, computerChoice) {
+function decideWinner(playerchoice, computerChoice) {
 
   let winner;
-  if (choice === computerChoice) {
+  if (playerchoice === computerChoice) {
     winner = 'Tie';
-  } else if (WINNING_POSSIBILITIES[choice].includes(computerChoice)) {
+  } else if (CHOICES[playerchoice].beats.includes(computerChoice)) {
     winner = 'You';
   } else {
     winner = 'Computer';
@@ -46,76 +44,120 @@ function printWinner(result) {
 
 }
 
-function getPlayerChoice() {
+function getPlayerChoice() { 
+   
+  let invalidChoice = true;
   prompt(`Choose one:(r)rock' ,(p)paper' ,(sc)scissors ,(l)lizard' ,(sp)spock'`);
-  let choiceKey = readline.question();
-  //player choice vaildation
-  while (!VALID_CHOICES_KEYS.includes(choiceKey)) {
-    prompt("That's not a valid choice");
-    choiceKey = readline.question();
+  let playerChoice = readline.question();
+  for(let key in CHOICES){
+    if(CHOICES[key].shorthand.includes(playerChoice)){
+      playerChoice = key;
+      invalidChoice = false;
+      break;
+    }
   }
-  let playerChoice = VALID_CHOICES[choiceKey];
-  return playerChoice;
 
+  while(invalidChoice) {
+     prompt('You need to input a valid choice!');
+     playerChoice = readline.question();
+      for(let key in CHOICES){
+        if(CHOICES[key].shorthand.includes(playerChoice)){
+          playerChoice = key;
+          invalidChoice = false;
+          break;
+        }
+    }
+  }
+
+  return playerChoice;
 }
 
 function getComputerChoice() {
-
-  // random generate computer choices
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES_KEYS.length);
-  let computerChoiceKey = VALID_CHOICES_KEYS[randomIndex];
-  let computerChoice = VALID_CHOICES[computerChoiceKey];
-  return computerChoice;
-
+  let propertiesOfCHOICES = Object.keys(CHOICES);
+  let randomIndex = Math.floor(Math.random() * propertiesOfCHOICES.length);
+  return propertiesOfCHOICES[randomIndex];
 }
 
 function printFinalWinnerAndTerminateGame(playerCount,computerCount) {
-
-  let flip;
-  if (playerCount === 3) {
-    flip = true;
+  let finish;
+  if (playerCount === Max_Score) {
+    finish = true;
     prompt(`You are the grand winner!`);
     prompt(`Game finished!!!`);
-  } else if (computerCount === 3 ) {
+  } else if (computerCount === Max_Score) {
     prompt(`Computer is the grand winner!`);
     prompt(`Game finished!!!`);
-    flip = true;
+    finish = true;
   } else {
-    flip = false;
+    finish = false;
   }
-  return flip;
+  return finish;
 }
 
 function gameScoreTracker(winner) {
   if (winner.toUpperCase() === 'YOU') {
-    playerScore++;
+    score['player'] ++;
   } else if (winner.toUpperCase() === "COMPUTER") {
-    computerScore++;
+    score['computer'] ++;
   }
 }
-//clear the screen before start the game
-console.clear();
-prompt('---Welcome to play BEST OF 5 (rock, paper, scissors,lizard and spock)!!!!---');
 
-while (true) {
-
-  roundNum++;
-  prompt(`Round ${roundNum}`);
-  let humanChoice = getPlayerChoice();
-  let computerChoice = getComputerChoice();
-
-  prompt(`You chose ${humanChoice}, computer choses ${computerChoice}`);
-
-  let winnerForTheRound = decideWinner(humanChoice,computerChoice);
-
-  printWinner(winnerForTheRound);
-  gameScoreTracker(winnerForTheRound);
-
-  prompt(`Your current score is: ${playerScore}`);
-  prompt(`Computer current score is: ${computerScore}\n`);
-
-  if (printFinalWinnerAndTerminateGame(playerScore,computerScore)) {
-    break;
-  }
-
+function printPlayerScore () {
+  prompt(`Your current score is: ${score['player']}\tComputer current score is: ${score['computer']}\n`);
 }
+
+function resetGame(){
+   score['player'] = 0;
+   score['computer'] = 0;
+   roundNum = 0;
+}
+
+function playAnotherGame(){
+  prompt('Do you want to player another round of the game? y(yes)/n(no)');
+  let answer = readline.question().toLowerCase();
+
+  while(answer !== 'y' && answer !== 'yes' && 
+      answer !== 'n' && answer !== 'no' ) {
+      prompt('It\'s not a valid choice!');
+      answer = readline.question().toLowerCase();
+  }
+  return answer; 
+}
+
+function startGame() {
+  console.clear();
+  prompt('---Welcome to play BEST OF 5 (rock, paper, scissors,lizard and spock)!!!!---');
+  do {
+
+    roundNum++;
+    prompt(`Round ${roundNum}`);
+    let humanChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    prompt(`You chose ${humanChoice}, computer choses ${computerChoice}`);
+
+    let winnerForTheRound = decideWinner(humanChoice,computerChoice);
+    printWinner(winnerForTheRound);
+    gameScoreTracker(winnerForTheRound);
+    printPlayerScore();
+    
+  }while(!printFinalWinnerAndTerminateGame(score['player'],score['computer']));
+}
+
+let answer;
+do {
+
+  resetGame();
+  startGame();
+  answer = playAnotherGame();
+
+  if (answer === 'n' || answer === 'no'){
+    prompt('Thanks for using the app! Goodbye!');
+  }
+  
+}while (answer === 'y' || answer === 'yes');
+
+
+
+
+
